d38399ef8c3dff3cf898fa2939cbb698f21911f4
TravisTroiani <travis.troiani@gmail.com>
2024-05-28 21:07:20 -0400

    new stuff

--- index.js
+++ index.js
@@ -2,3 +2,76 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide contribution guidelines:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test instructions:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    },
+];
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Successfully created README.md!')
+    );
+}
+function renderLicenseBadge(license) {
+    if (license === 'None') {
+        return '';
+    }
+    return `![License: ${license}](https://img.shields.io/badge/License-${license.replace(' ', '_')}-blue.svg)`;
+}
+
+function renderLicenseLink(license) {
+    if (license === 'None') {
+        return '';
+    }
+    return `\n- [License](#license)\n`;
+}
+
+function renderLicenseSection(license) {
+    if (license === 'None') {
+        return '';
+    }
+    return `## License\nThis project is licensed under the ${license} license.`;
+}
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,35 @@
+function generateMarkdown(data) {
+    return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)${renderLicenseLink(data.license)}
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, please contact me at [${data.email}](mailto:${data.email}). You can also find more of my work at [${data.github}](https://github.com/${data.github}).
+`;
+}
+
+module.exports = generateMarkdown;
